94a995a1828b28ca082c4ca13be326a5fef5d323
Andre Furlan <ensismoebius@gmail.com>
2019-04-21 16:52:15 -0300

    Hard work

--- src/CIN2019-Exercicio01.cpp
+++ src/CIN2019-Exercicio01.cpp
@@ -6,6 +6,7 @@
  */
 
 #include <cmath>
+#include <cstdio>
 #include <cstdlib>
 #include <iostream>
 #include <limits>
@@ -16,7 +17,7 @@
 
 class Gen {
 private:
-	double globalScore = std::numeric_limits<double>::max();
+	double globalScore;
 
 	int generation = 0;
 
@@ -38,32 +39,23 @@ private:
 	double* arrRankingSpaceFitness;
 
 	double (*fitnessFunction)(char* arrSubject, int subjectSize);
-	void (*printFunction)(char* arrPopulation, int populationSize, int subjectSize, int generation, int globalScore);
+	char* (*genesisFunction)(int populationSize, int subjectSize);
+	void (*printFunction)(char* arrPopulation, int populationSize, int subjectSize, int generation, double globalScore);
 
 	static const unsigned int bestBreederIndex = 0;
 	static const unsigned int worseBreederIndex = 2;
 	static const unsigned int bestRankSpacedBreederIndex = 1;
 
+	char* createPopulation() {
+		return (*this->genesisFunction)(this->populationSize, this->subjectSize);
+	}
+
 	double calculateIndividualFitness(char* arrSubject) {
-		double score = (*this->fitnessFunction)(arrSubject, this->subjectSize);
-		this->globalScore += score;
-		return score;
+		return (*this->fitnessFunction)(arrSubject, this->subjectSize);
 	}
 
 	double calculateRankingSpaceFitness(int populationIndex, double diversity) {
 		return diversity * this->diversityWeight + this->arrFitness[populationIndex] + this->fitnessWeight;
-	}
-
-	char *createPopulation() {
-		int genesAmount = this->populationSize * this->subjectSize;
-		char* population = new char[genesAmount];
-
-		// initializes the population
-		while (genesAmount--) {
-			population[genesAmount] = random() > RAND_MAX / 2 ? 1 : 0;
-		}
-
-		return population;
 	}
 
 	void doCrossOver(char* rna1, char* rna2) {
@@ -80,10 +72,14 @@ private:
 
 	void mutate(char* rna1, char* rna2) {
 		int rnaLocusIndex;
-		rnaLocusIndex = this->createRandomNumber() * this->subjectSize / 2;
-		rna1[rnaLocusIndex] = rna1[rnaLocusIndex] == 0 ? 1 : 0;
-		rnaLocusIndex = this->createRandomNumber() * this->subjectSize / 2;
-		rna2[rnaLocusIndex] = rna1[rnaLocusIndex] == 0 ? 1 : 0;
+		int amountOfMutations = this->createRandomNumber() * 10;
+
+		while (amountOfMutations--) {
+			rnaLocusIndex = this->createRandomNumber() * this->subjectSize / 2;
+			rna1[rnaLocusIndex] = rna1[rnaLocusIndex] == 0 ? 1 : 0;
+			rnaLocusIndex = this->createRandomNumber() * this->subjectSize / 2;
+			rna2[rnaLocusIndex] = rna2[rnaLocusIndex] == 0 ? 1 : 0;
+		}
 	}
 
 	char* extractRNA(char* subject, char chainNumber) {
@@ -171,6 +167,16 @@ private:
 			createNewSubject(newPopulation + ((*newPopulationIndex) * this->subjectSize), rnaBest2, rnaScpR2);
 			(*newPopulationIndex)++;
 		}
+
+		delete[] rnaBest1;
+		delete[] rnaBest2;
+		delete[] rnaSpcR1;
+		delete[] rnaScpR2;
+
+		rnaBest1 = NULL;
+		rnaBest2 = NULL;
+		rnaSpcR1 = NULL;
+		rnaScpR2 = NULL;
 	}
 
 	void makeWorseHaveSex(int* newPopulationIndex, char* newPopulation) {
@@ -205,7 +211,7 @@ private:
 	}
 
 public:
-	Gen(int subjectSize, int populationSize, double fitnessWeight, double diversityWeight, double crossoverRate, double mutationRate, double (*fitnessFunction)(char*, int), void (*printFunction)(char* arrPopulation, int populationSize, int subjectSize, int generation, int globalScore)) {
+	Gen(int subjectSize, int populationSize, double fitnessWeight, double diversityWeight, double crossoverRate, double mutationRate, double (*fitnessFunction)(char*, int), void (*printFunction)(char* arrPopulation, int populationSize, int subjectSize, int generation, double globalScore), char* (*genesisFunction)(int populationSize, int subjectSize)) {
 
 		if (populationSize < 4 || subjectSize < 2 || fitnessWeight <= 0 || diversityWeight <= 0 || crossoverRate > 1 || fitnessFunction == NULL) {
 			throw std::invalid_argument("must subjectSize > 1 \n must populationSize > 3 \n must fitnessWeight > 0 \n must diversityWeight > 0\n must crossoverRate < 1\n fitnessFunction != NULL \n ");
@@ -220,9 +226,10 @@ public:
 		this->subjectSize = subjectSize;
 		this->populationSize = populationSize;
 
-		// The print and fitness function
+		// The functions
 		this->printFunction = printFunction;
 		this->fitnessFunction = fitnessFunction;
+		this->genesisFunction = genesisFunction;
 
 		// Set the normalized values
 		this->fitnessWeight = fitnessWeight / (fitnessWeight + diversityWeight);
@@ -231,6 +238,8 @@ public:
 		this->arrFitness = new double[populationSize];
 		this->arrBreedersIndexes = new int[populationSize / 2];
 		this->arrRankingSpaceFitness = new double[populationSize];
+
+		this->globalScore = std::numeric_limits<double>::max();
 
 		this->arrPopulation = createPopulation();
 	}
@@ -269,18 +278,40 @@ public:
 	void evaluateAllSubjects() {
 
 		int index = 0;
-		int sumOfAllScores = 0;
 		double diversity;
+		double sumOfAllScores = 0;
+		char sumReachesInfinty = 0;
 
 		// evaluating all subjects
 		for (; index < this->populationSize; index++) {
 			sumOfAllScores += this->arrFitness[index] = this->calculateIndividualFitness(this->arrPopulation + (index * this->subjectSize));
 		}
 
+		// setting the global score, ideally, in most cases, it has to reach zero
+		if (sumOfAllScores == std::numeric_limits<double>::infinity()) {
+			this->globalScore = sumOfAllScores = std::numeric_limits<double>::max();
+			sumReachesInfinty = 1;
+		}
+
 		// normalizing results
 		index--;
-		for (; index >= 0; index--) {
-			this->arrFitness[index] /= sumOfAllScores;
+
+		if (sumReachesInfinty) {
+			// sumOfAllScores reaching infinity means
+			// no fitness, remember the lower the fitness
+			// value the better
+			for (; index >= 0; index--) {
+
+				if (this->arrFitness[index] == std::numeric_limits<double>::max()) {
+					this->arrFitness[index] = 1 / this->populationSize;
+					continue;
+				}
+				this->arrFitness[index] /= std::numeric_limits<double>::max();
+			}
+		} else {
+			for (; index >= 0; index--) {
+				this->arrFitness[index] /= sumOfAllScores;
+			}
 		}
 
 		// calculating the diversity and raking space fitness
@@ -372,7 +403,6 @@ public:
 		this->generation++;
 		delete[] this->arrPopulation;
 		this->arrPopulation = arrNewPopulation;
-		this->globalScore = 0;
 	}
 
 	void printPopulation() {
@@ -380,19 +410,60 @@ public:
 	}
 };
 
-void print(char* arrPopulation, int populationSize, int subjectSize, int generation, int score) {
+void print1(char* arrPopulation, int populationSize, int subjectSize, int generation, double score) {
 	std::cout << "Generation: " << generation << " Score:" << score << std::endl;
 	if (score > 0) return;
 
-//	for (int i = 0; i < populationSize; i++) {
-//		for (int j = 0; j < subjectSize; j++) {
-//			std::cout << (int) arrPopulation[(i * subjectSize) + j] << ",";
-//			if ((j + 1) % 3 == 0) std::cout << std::endl;
-//		}
-//		std::cout << std::endl;
-//	}
+	for (int i = 0; i < populationSize; i++) {
+		for (int j = 0; j < subjectSize; j++) {
+			std::cout << (int) arrPopulation[(i * subjectSize) + j] << ",";
+			if ((j + 1) % 3 == 0) std::cout << std::endl;
+		}
+		std::cout << std::endl;
+	}
 }
 
+void print2(char* arrPopulation, int populationSize, int subjectSize, int generation, double score) {
+	std::cout << "Generation: " << generation << " Score:" << score << std::endl;
+	//if (score > 0) return;
+
+	for (int i = 0; i < populationSize; i++) {
+		for (int j = 0; j < subjectSize; j++) {
+			std::cout << (int) arrPopulation[(i * subjectSize) + j] << ",";
+		}
+		std::cout << std::endl;
+	}
+}
+
+char* genesisFunction2(int populationSize, int subjectSize) {
+	int genesAmount = populationSize * subjectSize;
+	int i;
+
+	char* population = new char[genesAmount];
+
+	char* firstPop = convertFloatToIEEE754BinaryArray(0.0);
+	char* seconPop = convertFloatToIEEE754BinaryArray(1.0);
+
+	for (i = genesAmount - subjectSize - 1; i < genesAmount; i++) {
+		population[i] = firstPop[i];
+	}
+
+	genesAmount = genesAmount - subjectSize;
+
+	for (i = genesAmount - subjectSize - 1; i < genesAmount; i++) {
+		population[i] = seconPop[i];
+	}
+
+	genesAmount = genesAmount - subjectSize;
+
+	while (genesAmount--) {
+		population[genesAmount] = 0;
+	}
+
+	return population;
+}
+
+//subject size must be 32
 double fitness2(char* arrSubject, int subjectSize) {
 	float x = convertIEEE754BinaryArrayToFloat(arrSubject);
 
@@ -400,21 +471,29 @@ double fitness2(char* arrSubject, int subjectSize) {
 	double p2 = pow(sin(5 * M_PI * x), 6);
 	double final = (double) pow(2, p1) * p2;
 
-	if (final <= 0) return std::numeric_limits<float>::max();
+	if (final <= 0) return std::numeric_limits<double>::max();
 
 	return std::numeric_limits<double>::max() - final;
 
 }
 
-double fitness(char* arrSubject, int subjectSize) {
+char* genesisFunction1(int populationSize, int subjectSize) {
+	int genesAmount = populationSize * subjectSize;
+	char* population = new char[genesAmount];
+
+	// initializes the population
+	while (genesAmount--) {
+		population[genesAmount] = random() > RAND_MAX / 2 ? 1 : 0;
+	}
+
+	return population;
+}
+
+// subjectSize must be 12
+double fitness1(char* arrSubject, int subjectSize) {
 	double score = 0;
 	int index = subjectSize;
 
-	// represents a 3x4 matrix
-	//	0, 1, 0,
-	//	0, 1, 0,
-	//	0, 1, 0,
-	//	0, 1, 0,
 	const char targetSubject[12] = { 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1 };
 
 	while (index--) {
@@ -422,26 +501,143 @@ double fitness(char* arrSubject, int subjectSize) {
 	}
 	return score;
 }
+
+//////////////////////////////////////////////////////////////////////////////////
+typedef union {
+
+	float f;
+	struct {
+
+		// Order is important.
+		// Here the members of the union data structure
+		// use the same memory (32 bits).
+		// The ordering is taken
+		// from the LSB to the MSB.
+		unsigned int mantissa :23;
+		unsigned int exponent :8;
+		unsigned int sign :1;
+
+	} raw;
+} myfloat;
+
+unsigned int* printBinary(int n, int i) {
+
+	unsigned int* res = new unsigned int[i];
+
+	int k;
+	for (k = i - 1; k >= 0; k--) {
+
+		if ((n >> k) & 1) {
+			res[i - 1 - k] = 1;
+		} else {
+			res[i - 1 - k] = 0;
+		}
+	}
+
+	return res;
+}
+
+// Function to convert real value
+// to IEEE floating point representation
+unsigned int* printIEEE(float value) {
+
+	unsigned int* res = new unsigned int[32];
+
+	// Instantiate the union
+	myfloat var;
+
+	// Get the real value
+	var.f = value;
+
+	// Prints the IEEE 754 representation
+	// of a float value (32 bits)
+
+	res[0] = (printBinary(var.raw.sign, 1))[0];
+
+	unsigned int* expo = printBinary(var.raw.exponent, 8);
+	for (int i = 0; i < 8; i++) {
+		res[i + 1] = expo[i];
+	}
+
+	unsigned int* mantissa = printBinary(var.raw.mantissa, 23);
+	for (int i = 0; i < 23; i++) {
+		res[i + 9] = mantissa[i];
+	}
+
+	delete[] expo;
+	delete[] mantissa;
+
+	expo = NULL;
+	mantissa = NULL;
+
+	return res;
+}
+
+// Function to convert a binary array
+// to the corresponding integer
+unsigned int convertToInt(unsigned int* arr, unsigned int low, unsigned int high) {
+	unsigned int f = 0, i;
+	for (i = high; i >= low; i--) {
+		f = f + arr[i] * pow(2, high - i);
+	}
+	return f;
+}
+// Driver Code
 int main() {
 
-	int subjectSize = 32;
-	int populationSize = 4;
-	double fitnessWeight = 8;
-	double diversityWeight = 2;
-	double crossoverRate = 0.1;
-	double mutationRate = 0.1;
+	// Get the IEEE floating point representation
+	printf("IEEE 754 representation of %f is : \n", -2.25);
+	unsigned int* ieee = new unsigned int[32];
 
-	Gen pop = Gen(subjectSize, populationSize, fitnessWeight, diversityWeight, crossoverRate, mutationRate, fitness2, print);
-	pop.printPopulation();
+	ieee = printIEEE(-2.25);
 
-	while (pop.getGlobalScore() > 0) {
-		pop.evaluateAllSubjects();
-		pop.selectTheBestSubjects();
-		pop.printPopulation();
+	myfloat var;
 
-		pop.createNextGeneration();
-	}
+	// Convert the least significant
+	// mantissa part (23 bits)
+	// to corresponding decimal integer
+	unsigned int f = convertToInt(ieee, 9, 31);
+
+	// Assign integer representation of mantissa
+	var.raw.mantissa = f;
+
+	// Convert the exponent part (8 bits)
+	// to a corresponding decimal integer
+	f = convertToInt(ieee, 1, 8);
+
+	// Assign integer representation
+	// of the exponent
+	var.raw.exponent = f;
+
+	// Assign sign bit
+	var.raw.sign = ieee[0];
+
+	printf("The float value of the given"
+			" IEEE-754 representation is : \n");
+	printf("%f", var.f);
 
 	return 0;
 }
+
+//	char* firstPop = convertFloatToIEEE754BinaryArray(5.24);
+//	float teste = convertIEEE754BinaryArrayToFloat(firstPop);
+
+//	int subjectSize = 32;
+//	int populationSize = 4;
+//	double fitnessWeight = 8;
+//	double diversityWeight = 2;
+//	double crossoverRate = 0.7;
+//	double mutationRate = 0.6;
+//
+//	Gen pop = Gen(subjectSize, populationSize, fitnessWeight, diversityWeight, crossoverRate, mutationRate, fitness2, print2, genesisFunction2);
+//
+//	while (pop.getGlobalScore() > 0) {
+//		pop.printPopulation();
+//
+//		pop.evaluateAllSubjects();
+//		pop.selectTheBestSubjects();
+//		pop.createNextGeneration();
+//	}
+//
+//	pop.printPopulation();
 

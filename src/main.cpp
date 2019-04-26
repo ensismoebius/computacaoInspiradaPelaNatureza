97298e77556cf5e60b5a77e1d8fa4587a5542bfc
Andre Furlan <ensismoebius@gmail.com>
2019-04-26 06:37:27 -0300

    Just some renaming

--- src/main.cpp
+++ src/main.cpp
@@ -23,24 +23,38 @@
 #include "hillClimbingAnnealingExperiments/Experiment04.cpp"
 #include "hillClimbingAnnealingExperiments/Experiment05.cpp"
 
-/**
- * Do all the experiments!!!!!
- */
-int main() {
-
+void doGeneticExperiments() {
+	// genetic algorithms
 	geneticExperiments::Experiment01::perform(10000, "/tmp/geneticExperiments::Experiment01.csv");
 	geneticExperiments::Experiment02::perform(10000, "/tmp/geneticExperiments::Experiment02.csv");
 	geneticExperiments::Experiment03::perform(10000, "/tmp/geneticExperiments::Experiment03.csv");
 	geneticExperiments::Experiment04::perform(10000, "/tmp/geneticExperiments::Experiment04.csv");
+}
 
+void doHillClimbingExperiments() {
 	// hill climbing algorithms
 	hillClimbingAnnealingExperiments::Experiment01::perform(10000, "/tmp/hillClimbingAnnealingExperiments::Experiment01.csv");
 	hillClimbingAnnealingExperiments::Experiment02::perform(10000, "/tmp/hillClimbingAnnealingExperiments::Experiment02.csv");
 	hillClimbingAnnealingExperiments::Experiment03::perform(10000, "/tmp/hillClimbingAnnealingExperiments::Experiment03.csv");
 	hillClimbingAnnealingExperiments::Experiment05::perform(10000, "/tmp/hillClimbingAnnealingExperiments::Experiment05.csv");
+}
 
+void doAnnealingExperiments() {
 	// annealing algorithms
 	hillClimbingAnnealingExperiments::Experiment04::perform(10000, "/tmp/hillClimbingAnnealingExperiments::Experiment04.csv");
+}
+
+/**
+ * Do all the experiments!!!!!
+ */
+int main() {
+
+	// genetic algorithms
+	doGeneticExperiments();
+	// hill climbing algorithms
+	doHillClimbingExperiments();
+	// annealing algorithms
+	doAnnealingExperiments();
 
 	return 0;
 }
